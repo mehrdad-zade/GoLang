8a985e975c2c46ff4dfb8d274a00f5cad9947de8
ACmalemzadeh <mehrdad.alemzadeh@aletha.ca>
2022-10-06 09:41:01 -0400

    added more examples

--- Main.go
+++ Main.go
@@ -16,19 +16,36 @@ func main() {
 	var w Writer = ConsoleWriter{}
 	w.Write([]byte("Let's Go"))
 
+	//Example 2
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i:=0; i<10; i++ {
+		fmt.Println(inc.Increment())
+	}
 
 }
 
-//-------------------------------Interfaces
-//Example 1
-type Writer interface { //interfaces are data containers; they don't describe data, they describe behaviours
+// -------------------------------Interfaces
+// Example 1
+type Writer interface { //interfaces don't describe data, they describe behaviours. structs define data types, interfaces define methods.
 	Write([]byte) (int, error)
 }
 
-type ConsoleWriter struct {} // a way to implement the interface
+type ConsoleWriter struct{} // a way to implement the interface
 
-func (cs ConsoleWriter) Write(data []byte) (int, error){
+func (cs ConsoleWriter) Write(data []byte) (int, error) { //instead of using something like "implements" keyword, we implicitly implement Writer interface by creating a method on our ConsoleWriter construct that has the signiture of a Writer interface
 	n, err := fmt.Println(string(data))
 	return n, err
 }
+
 //End of Example 1
+
+//Example 2
+type Incrementer interface {
+	Increment() int
+}
+type IntCounter int
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
